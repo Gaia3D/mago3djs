cd220479613125e42d46499c0abc8f065bf2c9b7
DESKTOP-49U0CRN\Cheon JeongDae <jdcheon@gaia3d.com>
2017-01-26 13:41:04 +0900

    파일 이름 리팩토링 후 테스트 케이스 추가

--- src/mago3d/Accessor.js
+++ src/mago3d/Accessor.js
@@ -3,22 +3,22 @@
 /**
  * 버퍼 안의 데이터를 어떻게 읽어야 할지 키가 되는 객체
  */
-var F4DAccessor = function() {
-	// new 를 사용하도록 강제해야 함
-	if(!(this instanceof F4DAccessor)) {
+var Accessor = function() {
+	
+	if(!(this instanceof Accessor)) {
 		throw new Error("이 객체는 new를 사용하여 생성해야 합니다.");
 	}
 		
-	this.buffer_id = undefined;
+	this.buffer_id;
 	// 0= position, 1= normal, 2= color, 3= texcoord.***
-	this.accesor_type = undefined;
-	this.buffer_start = undefined;
+	this.accesor_type;
+	this.buffer_start;
 	// 버퍼의 시작 시점
-	this.stride = undefined;
+	this.stride;
 	// character, int 등
-	this.data_ytpe = undefined;
+	this.data_ytpe;
 	// 2차원, 3차원
-	this.dimension = undefined;
+	this.dimension;
 	
 	// 데이터가 포함되어 있는 x,y,z의 한계를 바운드라고 한다. 바운드 좌표
 	this.min_x = 0.0;
