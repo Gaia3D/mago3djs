8b845effc617c1c8126e0b6aa3d9292757f3b98d
손성도 <sdson@gaia3d.com>
2017-03-08 15:56:31 +0900

    Asimetric Octree

--- src/mago3d/BlockModels.js
+++ src/mago3d/BlockModels.js
@@ -12,8 +12,10 @@ var Block = function() {
 	this.vBOVertexIdxCacheKeysContainer = new VBOVertexIdxCacheKeysContainer(); // Change this for "vbo_VertexIdx_CacheKeys_Container__idx".***
 	this.mIFCEntityType = -1;
 	this.isSmallObj = false;
-	  
+	this.radius = 10;  
 	this.vertex_count = 0; // only for test.*** delete this.***
+	
+	this.lego; // legoBlock.***
 };
 
 /**
@@ -92,6 +94,8 @@ BlocksList.prototype.parseArrayBuffer = function(gl, arrayBuffer, readWriter) {
 		if(maxLength < 1.0) block.isSmallObj = true;
 		else block.isSmallObj = false;
 		
+		block.radius = maxLength/2.0;
+		
 		// New for read multiple vbo datas (indices cannot superate 65535 value).***
 		var vboDatasCount = readWriter.readUInt32(arrayBuffer, bytesReaded, bytesReaded+4);
 		bytesReaded += 4;
@@ -152,6 +156,113 @@ BlocksList.prototype.parseArrayBuffer = function(gl, arrayBuffer, readWriter) {
 			bytesReaded = bytesReaded + 2*shortIndicesValues_count; // updating data.***
 			vbo_vi_cacheKey.indices_count = shortIndicesValues_count;  
 		}
+	}
+	this.fileLoadState = 4; // 4 = parsing finished.***
+};
+
+/**
+ * 어떤 일을 하고 있습니까?
+ * @param idx 변수
+ * @returns block
+ */
+BlocksList.prototype.parseArrayBufferAsimetricVersion = function(gl, arrayBuffer, readWriter) {
+	this.fileLoadState = 3;// 3 = parsing started.***
+	var bytesReaded = 0;
+	var blocksCount = readWriter.readUInt32(arrayBuffer, bytesReaded, bytesReaded + 4); 
+	bytesReaded += 4;
+	
+	for(var i=0; i<blocksCount; i++) {
+		var block = this.newBlock();
+		  
+		// 1rst, read bbox.***
+		var bbox = new BoundingBox();
+		bbox.minX = new Float32Array(arrayBuffer.slice(bytesReaded, bytesReaded+4));
+		bytesReaded += 4;
+		bbox.minY = new Float32Array(arrayBuffer.slice(bytesReaded, bytesReaded+4));
+		bytesReaded += 4;
+		bbox.minZ = new Float32Array(arrayBuffer.slice(bytesReaded, bytesReaded+4));
+		bytesReaded += 4;
+		  
+		bbox.maxX = new Float32Array(arrayBuffer.slice(bytesReaded, bytesReaded+4));
+		bytesReaded += 4;
+		bbox.maxY = new Float32Array(arrayBuffer.slice(bytesReaded, bytesReaded+4));
+		bytesReaded += 4;
+		bbox.maxZ = new Float32Array(arrayBuffer.slice(bytesReaded, bytesReaded+4));
+		bytesReaded += 4;
+		
+		var maxLength = bbox.getMaxLength();
+		if(maxLength < 1.0) block.isSmallObj = true;
+		else block.isSmallObj = false;
+		
+		block.radius = maxLength/2.0;
+		
+		// New for read multiple vbo datas (indices cannot superate 65535 value).***
+		var vboDatasCount = readWriter.readUInt32(arrayBuffer, bytesReaded, bytesReaded+4);
+		bytesReaded += 4;
+		for(var j=0; j<vboDatasCount; j++) {
+		
+			// 1) Positions array.***************************************************************************************
+			var vertex_count = readWriter.readUInt32(arrayBuffer, bytesReaded, bytesReaded+4);
+			bytesReaded += 4;
+			var verticesFloatValues_count = vertex_count * 3;
+			
+			block.vertex_count = vertex_count;
+
+			var startBuff = bytesReaded;
+			var endBuff = bytesReaded + 4*verticesFloatValues_count;
+
+			var vbo_vi_cacheKey = block.vBOVertexIdxCacheKeysContainer.newVBOVertexIdxCacheKey();
+			vbo_vi_cacheKey.pos_vboDataArray = new Float32Array(arrayBuffer.slice(startBuff, endBuff));
+			
+			/*
+			vbo_vi_cacheKey.MESH_VERTEX_cacheKey = gl.createBuffer ();
+			gl.bindBuffer(gl.ARRAY_BUFFER, vbo_vi_cacheKey.MESH_VERTEX_cacheKey);
+			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(arrayBuffer.slice(startBuff, endBuff)), gl.STATIC_DRAW);
+			  */
+			bytesReaded = bytesReaded + 4*verticesFloatValues_count; // updating data.***
+			 
+			// 2) Normals.************************************************************************************************
+			vertex_count = readWriter.readUInt32(arrayBuffer, bytesReaded, bytesReaded+4);
+			bytesReaded += 4;
+			var normalByteValues_count = vertex_count * 3;
+			//Test.***********************
+			//for(var j=0; j<normalByteValues_count; j++)
+			//{
+			//	var value_x = readWriter.readInt8(arrayBuffer, bytesReaded, bytesReaded+1); bytesReaded += 1;
+			//}
+			startBuff = bytesReaded;
+			endBuff = bytesReaded + 1*normalByteValues_count;
+			
+			vbo_vi_cacheKey.nor_vboDataArray = new Int8Array(arrayBuffer.slice(startBuff, endBuff));
+			/*
+			vbo_vi_cacheKey.MESH_NORMAL_cacheKey = gl.createBuffer ();
+			gl.bindBuffer(gl.ARRAY_BUFFER, vbo_vi_cacheKey.MESH_NORMAL_cacheKey);
+			gl.bufferData(gl.ARRAY_BUFFER, new Int8Array(arrayBuffer.slice(startBuff, endBuff)), gl.STATIC_DRAW);
+			  */
+			bytesReaded = bytesReaded + 1*normalByteValues_count; // updating data.***
+			
+			// 3) Indices.*************************************************************************************************
+			var shortIndicesValues_count = readWriter.readUInt32(arrayBuffer, bytesReaded, bytesReaded+4);
+			bytesReaded += 4;
+			startBuff = bytesReaded;
+			endBuff = bytesReaded + 2*shortIndicesValues_count;
+			  
+			vbo_vi_cacheKey.idx_vboDataArray = new Int16Array(arrayBuffer.slice(startBuff, endBuff));
+			/*
+			vbo_vi_cacheKey.MESH_FACES_cacheKey= gl.createBuffer ();
+			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vbo_vi_cacheKey.MESH_FACES_cacheKey);
+			gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(arrayBuffer.slice(startBuff, endBuff)), gl.STATIC_DRAW);
+			 */ 
+			bytesReaded = bytesReaded + 2*shortIndicesValues_count; // updating data.***
+			vbo_vi_cacheKey.indices_count = shortIndicesValues_count;  
+		}
+		
+		// in asimetricVersion must load the block's lego.***
+		if(block.lego == undefined)
+			block.lego = new Lego();
+		
+		block.lego.fileLoadState = 2; // data is loaded with the blockModel.***
+		bytesReaded = block.lego.parseArrayBuffer(gl, readWriter, arrayBuffer, bytesReaded);
 	}
 	this.fileLoadState = 4; // 4 = parsing finished.***
 };
