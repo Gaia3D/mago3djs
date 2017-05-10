4eb97b2e4d30efbbb003a10e0385d22f2a55bdd8
임호준 <hjlim@gaia3d.com>
2017-05-10 11:29:23 +0900

    Jasmine testcode 임시컴잇

---
+++ test/MagoFacadeTest.js
@@ -0,0 +1,10 @@
+'use district';
+
+describe('MagoFacade 테스트', function() {
+	var api;
+	describe('Mago3D 상태 변경 API', function() {
+		it("Mago3D 활성화", function(){
+			changeMagoStateAPI(true);
+		})
+	});
+});
