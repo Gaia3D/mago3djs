cb00440fd28f21ead6ff78d5bf282ff5205ae7a0
sdson <sdson@gaia3d.com>
2017-09-29 10:02:50 +0900

    recommit

--- src/mago3d/core/MagoManager.js
+++ src/mago3d/core/MagoManager.js
@@ -5791,7 +5791,7 @@ MagoManager.prototype.makeHierachyTest = function()
 {
 	var motherNode = this.hierarchyManager.newMotherNode();
 	
-	//https://code.tutsplus.com/articles/data-structures-with-javascript-tree--cms-23393
+	
 	
 };
 
