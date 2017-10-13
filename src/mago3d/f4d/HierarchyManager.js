8b5d5ca202e69d7e46bd0feeab9f25dab4f5b5fa
sdson <sdson@gaia3d.com>
2017-10-13 17:09:32 +0900

    refactoring swapRenderingFase

--- src/mago3d/f4d/HierarchyManager.js
+++ src/mago3d/f4d/HierarchyManager.js
@@ -23,6 +23,31 @@ var HierarchyManager = function()
  * @class GeoLocationData
  * @param geoLocData 변수
  */
+HierarchyManager.prototype.getNodeByDataName = function(dataName, dataNameValue) 
+{
+	var nodesCount = this.nodesArray.length;
+	var i=0;
+	var find = false;
+	var node;
+	var resultNode;
+	while (!find && i<nodesCount)
+	{
+		node = this.nodesArray[i];
+		if (node.data[dataName] && node.data[dataName] == dataNameValue)
+		{
+			resultNode = node;
+		}
+		i++;
+	}
+	
+	return resultNode;
+};
+
+/**
+ * 어떤 일을 하고 있습니까?
+ * @class GeoLocationData
+ * @param geoLocData 변수
+ */
 HierarchyManager.prototype.newNode = function() 
 {
 	var node = new Node();
