4f29017d085e3895f0b683d542d69d1eef3b92cd
sdson <sdson@gaia3d.com>
2017-11-08 11:16:39 +0900

    commit

--- src/mago3d/core/ManagerFactory.js
+++ src/mago3d/core/ManagerFactory.js
@@ -349,11 +349,13 @@ var ManagerFactory = function(viewer, containerId, serverPolicy, projectIdArray,
 	{
 		// webgl lost events.******************************************
 		var canvas = document.getElementById(containerId);
-		canvas.addEventListener('webglcontextlost', function(e) {
+		canvas.addEventListener('webglcontextlost', function(e) 
+		{
 			console.log(e);
 		}, false);
 		
-		canvas.addEventListener('webglcontextrestored', function(e) {
+		canvas.addEventListener('webglcontextrestored', function(e) 
+		{
 		  console.log(e); 
 		}, false);
 		//-------------------------------------------------------------
