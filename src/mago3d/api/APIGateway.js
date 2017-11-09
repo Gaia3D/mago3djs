6e8d2eb971c30b863c821b0d5c75e25ab72624f1
DESKTOP-49U0CRN\Cheon JeongDae <jdcheon@gaia3d.com>
2017-11-09 18:18:54 +0900

    임시 커밋

--- src/mago3d/api/APIGateway.js
+++ src/mago3d/api/APIGateway.js
@@ -133,7 +133,7 @@ function changeColorAPI(managerFactoryInstance, projectId, dataKey, objectIds, p
  * location and rotation 변경
  * @param {ManagerFactory} managerFactoryInstance
  * @param {string} projectId
- * @param {string} data_key
+ * @param {string} dataKey
  * @param {string} latitude 위도
  * @param {string} longitude 경도
  * @param {string} height 높이
@@ -141,13 +141,13 @@ function changeColorAPI(managerFactoryInstance, projectId, dataKey, objectIds, p
  * @param {string} pitch 위, 아래
  * @param {string} roll 좌, 우 기울기
  */
-function changeLocationAndRotationAPI(managerFactoryInstance, projectId, data_key, latitude, longitude, height, heading, pitch, roll) 
+function changeLocationAndRotationAPI(managerFactoryInstance, projectId, dataKey, latitude, longitude, height, heading, pitch, roll) 
 {
 	if (managerFactoryInstance === null) { return; } 
 	
 	var api = new API("changeLocationAndRotation");
-	api.setDataKey(data_key);
 	api.setProjectId(projectId);
+	api.setDataKey(dataKey);
 	api.setLatitude(latitude);
 	api.setLongitude(longitude);
 	api.setElevation(height);
