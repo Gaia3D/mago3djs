3bd1d897014c6a1e400db7673e66468e528a474e
kwunjaehyun <shinekill3@gmail.com>
2019-05-27 15:34:21 +0900

    - geometry vertex type add annotation. Vertex.js VertexList.js VertexMatrix.js VtxProfile.js VtxRing.js VtxRingList.js VtxSegment.js IndexRange.js VtxProfileList.js ing...

--- src/mago3d/geometry/VtxSegment.js
+++ src/mago3d/geometry/VtxSegment.js
@@ -1,8 +1,13 @@
 'use strict';
 
 /**
- * 어떤 일을 하고 있습니까?
+ * Vertex segment.
+ * @exception {Error} Messages.CONSTRUCT_ERROR
+ * 
  * @class VtxSegment
+ * 
+ * @param {Vertex} startVertex Optional. start vertex.
+ * @param {Vertex} endVertex Optional. end vertex.
  */
 var VtxSegment = function(startVertex, endVertex) 
 {
@@ -11,7 +16,16 @@ var VtxSegment = function(startVertex, endVertex)
 		throw new Error(Messages.CONSTRUCT_ERROR);
 	}
 
+	/**
+	 * start vertex.
+	 * @type {Vertex}
+	 */
 	this.startVertex;
+
+	/**
+	 * end vertex.
+	 * @type {Vertex}
+	 */
 	this.endVertex;
 	
 	if (startVertex)
@@ -21,12 +35,22 @@ var VtxSegment = function(startVertex, endVertex)
 	{ this.endVertex = endVertex; }
 };
 
+/**
+ * set start, end vertex.
+ * @param {Vertex} startVertex 
+ * @param {Vertex} endVertex
+ */
 VtxSegment.prototype.setVertices = function(startVertex, endVertex)
 {
 	this.startVertex = startVertex;
 	this.endVertex = endVertex;
 };
 
+/**
+ * get direction between start vertex and end vertex.
+ * @param {Point3d} resultDirection if undefined , set new Point3D instance.
+ * @return {Point3d} resultDirection
+ */
 VtxSegment.prototype.getDirection = function(resultDirection)
 {
 	// the direction is an unitary vector.***
@@ -39,6 +63,11 @@ VtxSegment.prototype.getDirection = function(resultDirection)
 	return resultDirection;
 };
 
+/**
+ * get vector point between start vertex and end vertex.
+ * @param {Point3d} resultVector if undefined , set new Point3D instance.
+ * @return {Point3d} resultVector
+ */
 VtxSegment.prototype.getVector = function(resultVector)
 {
 	if (this.startVertex === undefined || this.endVertex === undefined)
@@ -54,6 +83,11 @@ VtxSegment.prototype.getVector = function(resultVector)
 	return resultVector;
 };
 
+/**
+ * get line between start vertex and end vertex.
+ * @param {Line} resultLine if undefined , set new Line instance.
+ * @return {Line} resultLine
+ */
 VtxSegment.prototype.getLine = function(resultLine)
 {
 	if (resultLine === undefined)
@@ -65,16 +99,41 @@ VtxSegment.prototype.getLine = function(resultLine)
 	return resultLine;
 };
 
+/**
+ * get squared length.
+ * @return {Number} squared length
+ * 
+ * @see Point3D#squareDistToPoint
+ */
 VtxSegment.prototype.getSquaredLength = function()
 {
 	return this.startVertex.point3d.squareDistToPoint(this.endVertex.point3d);
 };
 
+/**
+ * get length. 
+ * @return {Number} square root of squared length
+ */
 VtxSegment.prototype.getLength = function()
 {
 	return Math.sqrt(this.getSquaredLength());
 };
 
+/**
+ * get intersection info with point. 
+ * @param {Point3D} point
+ * @param {Number} error default is 10E-8.
+ * @return {Number} Constant.INTERSECTION_*
+ * Constant.INTERSECTION_OUTSIDE = 0
+ * Constant.INTERSECTION_INTERSECT= 1
+ * Constant.INTERSECTION_INSIDE = 2
+ * Constant.INTERSECTION_POINT_A = 3
+ * Constant.INTERSECTION_POINT_B = 4
+ * 
+ * @see Constant
+ * @see Line#isCoincidentPoint
+ * @see Point3D#distToPoint
+ */
 VtxSegment.prototype.intersectionWithPoint = function(point, error)
 {
 	// check if the point intersects the vtxSegment's line.***
@@ -85,12 +144,6 @@ VtxSegment.prototype.intersectionWithPoint = function(point, error)
 	
 	if (!line.isCoincidentPoint(point, error))
 	{ return Constant.INTERSECTION_OUTSIDE; } // no intersection.***
-	
-	//Constant.INTERSECTION_OUTSIDE = 0;
-	//Constant.INTERSECTION_INTERSECT= 1;
-	//Constant.INTERSECTION_INSIDE = 2;
-	//Constant.INTERSECTION_POINT_A = 3;
-	//Constant.INTERSECTION_POINT_B = 4;
 	
 	// now, check if is inside of the segment or if is coincident with any vertex of segment.***
 	var distA = this.startVertex.point3d.distToPoint(point);
@@ -109,8 +162,7 @@ VtxSegment.prototype.intersectionWithPoint = function(point, error)
 	}
 	
 	if (Math.abs(distA + distB - distTotal) < error)
-	{ return Constant.INTERSECTION_INSIDE; }
-	
+	{ return Constant.INTERSECTION_INSIDE; }	
 };
 
 
