ebac30e73a128d4baaf928552979d370d7b3a3b0
박진우 <jwpark@gaia3d.com>
2019-06-14 11:00:03 +0900

    Camera, AnimationManager Test 코드 작성

--- src/mago3d/core/AnimationManager.js
+++ src/mago3d/core/AnimationManager.js
@@ -1,45 +1,6 @@
 'use strict';
 
 /**
- * manage AnimationData
- * @class AnimationData
- */
-var AnimationData = function() 
-{
-	if (!(this instanceof AnimationData)) 
-	{
-		throw new Error(Messages.CONSTRUCT_ERROR);
-	}
-	
-	this.birthTime;
-	this.lastTime; // the last update time.
-	this.durationInSeconds;
-	
-	// start location.
-	this.startLongitude;
-	this.startLatitude;
-	this.startAltitude;
-	
-	// target location.
-	this.targetLongitude;
-	this.targetLatitude;
-	this.targetAltitude;
-	
-	// target rotation.
-	this.targetHeading;
-	this.targetPitch;
-	this.targetRoll;
-	
-	// linear velocity in m/s.
-	this.linearVelocityInMetersSecond;
-	
-	// angular velocity deg/s.
-	this.headingAngDegSecondVelocity;
-	this.pitchAngDegSecondVelocity;
-	this.rollAngDegSecondVelocity;
-};
-
-/**
  * AnimationManager
  * @class AnimationManager
  */
@@ -49,13 +10,13 @@ var AnimationManager = function()
 	{
 		throw new Error(Messages.CONSTRUCT_ERROR);
 	}
-	
-	this.nodesMap;
-	
+
+	this.nodesMap;	
 };
 
 /**
  * put the node which will move
+ * @param {Node} node
  */
 AnimationManager.prototype.putNode = function(node) 
 {
