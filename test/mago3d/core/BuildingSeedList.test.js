98dfd232f397044f627e055ab19c04a58e01d8c1
kwunjaehyun <shinekill3@gmail.com>
2019-07-08 12:05:44 +0900

    - 테스트 데이터 사용 환경 추가  -- 카르마에 proxy 추가 /resource 로 접근하면 /test/gaia3d/resource 폴더로 접근됨.  -- 테스트 코드사용하시면 됨.

--- test/mago3d/core/BuildingSeedList.test.js
+++ test/mago3d/core/BuildingSeedList.test.js
@@ -2,72 +2,71 @@
 
 describe("특정 데이터(vehicle, F4D_AutonomousBus)를 대상으로 BuildingSeedList 테스트, 사전작업으로 objectIndexFile 로드", function () 
 {
-	// var seedList;
+	var seedList;
+	
+	var path = './resource/f4d_sample/';
+	var geometrySubDataPath = 'vehicle';
+	var dummyCacheVersion = undefined;
+	var fileName = path + geometrySubDataPath + Constant.OBJECT_INDEX_FILE + Constant.CACHE_VERSION + dummyCacheVersion;
+	beforeAll(function()
+	{
+		return loadWithXhr(fileName).then(function(res) 
+		{
+			seedList = new BuildingSeedList();
+			//Promise 정상 수행됬을 때 ArrayBuffer 할당.
+			seedList.dataArrayBuffer = res;
+		});
+	});
 
-	// var path = 'http://localhost';
-	// var geometrySubDataPath = 'vehicle';
-	// var dummyCacheVersion = undefined;
-	// var fileName = path + "/f4d/" + geometrySubDataPath + Constant.OBJECT_INDEX_FILE + Constant.CACHE_VERSION + dummyCacheVersion;
-
-	// beforeAll(function()
-	// {
-	// 	return loadWithXhr(fileName).then(function(res) 
-	// 	{
-	// 		seedList = new BuildingSeedList();
-	// 		//Promise 정상 수행됬을 때 ArrayBuffer 할당.
-	// 		seedList.dataArrayBuffer = res;
-	// 	});
-	// });
-
-	// it('완료 후 dataArrayBuffer 할당 체크', function() 
-	// {	
-	// 	expect(seedList.dataArrayBuffer).toBeDefined();
-	// });
-	// describe('parseBuildingSeedArrayBuffer 테스트', function() 
-	// {
-	// 	var correctSeedCnt = 1;
-	// 	var correctSeedId = 'AutonomousBus';
-	// 	var correctSeedFileName = 'F4D_AutonomousBus';
-	// 	var correctLon = 128.593909;
-	// 	var correctLan = 34.903708;
-	// 	var correctAlt = 10;
-	// 	var correctMinX = -1.1302670240402222;
-	// 	var correctMinY = -2.580000162124634;
-	// 	var correctMinZ = -1.2879274929344546e-16;
-	// 	var correctMaxX = 1.1302670240402222;
-	// 	var correctMaxY = 2.580000162124634;
-	// 	var correctMaxZ = 2.42134952545166;
+	it('완료 후 dataArrayBuffer 할당 체크', function() 
+	{	
+		expect(seedList.dataArrayBuffer).toBeDefined();
+	});
+	describe('parseBuildingSeedArrayBuffer 테스트', function() 
+	{
+		var correctSeedCnt = 1;
+		var correctSeedId = 'AutonomousBus';
+		var correctSeedFileName = 'F4D_AutonomousBus';
+		var correctLon = 128.593909;
+		var correctLan = 34.903708;
+		var correctAlt = 10;
+		var correctMinX = -1.1302670240402222;
+		var correctMinY = -2.580000162124634;
+		var correctMinZ = -1.2879274929344546e-16;
+		var correctMaxX = 1.1302670240402222;
+		var correctMaxY = 2.580000162124634;
+		var correctMaxZ = 2.42134952545166;
 		
-	// 	beforeAll(function() 
-	// 	{
-	// 		seedList.parseBuildingSeedArrayBuffer();
-	// 	});
+		beforeAll(function() 
+		{
+			seedList.parseBuildingSeedArrayBuffer();
+		});
 		
-	// 	it('buildingseed length check', function() 
-	// 	{
-	// 		var buildSeedLength = seedList.getBuildingSeedLength();
-	// 		expect(buildSeedLength).toEqual(correctSeedCnt);
-	// 	});
+		it('buildingseed length check', function() 
+		{
+			var buildSeedLength = seedList.getBuildingSeedLength();
+			expect(buildSeedLength).toEqual(correctSeedCnt);
+		});
 
-	// 	it('buildingseed 속성 check', function() 
-	// 	{
-	// 		for (var i=0, len = seedList.getBuildingSeedLength(); i < len; i++) 
-	// 		{
-	// 			var buildingSeed = seedList.getBuildingSeed(i);
-	// 			expect(buildingSeed.buildingId).toEqual(correctSeedId);
-	// 			expect(buildingSeed.buildingFileName).toEqual(correctSeedFileName);
-	// 			expect(buildingSeed.geographicCoord.longitude).toEqual(correctLon);
-	// 			expect(buildingSeed.geographicCoord.latitude).toEqual(correctLan);
-	// 			expect(buildingSeed.geographicCoord.altitude).toEqual(correctAlt);
-	// 			expect(buildingSeed.bBox.minX).toEqual(correctMinX);
-	// 			expect(buildingSeed.bBox.minY).toEqual(correctMinY);
-	// 			expect(buildingSeed.bBox.minZ).toEqual(correctMinZ);
-	// 			expect(buildingSeed.bBox.maxX).toEqual(correctMaxX);
-	// 			expect(buildingSeed.bBox.maxY).toEqual(correctMaxY);
-	// 			expect(buildingSeed.bBox.maxZ).toEqual(correctMaxZ);
-	// 		}
-	// 	});
-	// });
+		it('buildingseed 속성 check', function() 
+		{
+			for (var i=0, len = seedList.getBuildingSeedLength(); i < len; i++) 
+			{
+				var buildingSeed = seedList.getBuildingSeed(i);
+				expect(buildingSeed.buildingId).toEqual(correctSeedId);
+				expect(buildingSeed.buildingFileName).toEqual(correctSeedFileName);
+				expect(buildingSeed.geographicCoord.longitude).toEqual(correctLon);
+				expect(buildingSeed.geographicCoord.latitude).toEqual(correctLan);
+				expect(buildingSeed.geographicCoord.altitude).toEqual(correctAlt);
+				expect(buildingSeed.bBox.minX).toEqual(correctMinX);
+				expect(buildingSeed.bBox.minY).toEqual(correctMinY);
+				expect(buildingSeed.bBox.minZ).toEqual(correctMinZ);
+				expect(buildingSeed.bBox.maxX).toEqual(correctMaxX);
+				expect(buildingSeed.bBox.maxY).toEqual(correctMaxY);
+				expect(buildingSeed.bBox.maxZ).toEqual(correctMaxZ);
+			}
+		});
+	});
 });
 
 describe('BuildingSeedList', function () 
