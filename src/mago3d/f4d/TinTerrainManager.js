0f51caa76b31e20d3a083a487cb9d9a20d2c05c6
sdson <sdson@gaia3d.com>
2020-04-16 14:53:15 +0900

    change depth

--- src/mago3d/f4d/TinTerrainManager.js
+++ src/mago3d/f4d/TinTerrainManager.js
@@ -10,7 +10,7 @@ var TinTerrainManager = function(options)
 		throw new Error(Messages.CONSTRUCT_ERROR);
 	}
 
-	this.maxDepth = 15;
+	this.maxDepth = 17;
 	this.currentVisibles_terrName_geoCoords_map = {}; // current visible terrains map[terrainPathName, geographicCoords].
 	this.currentTerrainsMap = {}; // current terrains (that was created) map[terrainPathName, tinTerrain].
 	
