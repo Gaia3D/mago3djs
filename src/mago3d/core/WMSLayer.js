5883060f046813975a1efc43b6726a013a9bc858
sdson <sdson@gaia3d.com>
2020-04-24 12:08:51 +0900

    WMSLayer tile extension in mercator coords

--- src/mago3d/core/WMSLayer.js
+++ src/mago3d/core/WMSLayer.js
@@ -12,7 +12,7 @@ var WMSLayer = function(options)
 	this.filter = defaultValue(options.filter, undefined);
 
 	var maxZoom = defaultValue(options.maxZoom, 18);
-	var minZoom = defaultValue(options.minZoom, 3);
+	var minZoom = defaultValue(options.minZoom, 0);
 	
 	Object.defineProperty(this, 'maxZoom', {
 		get: function()
@@ -41,10 +41,10 @@ var WMSLayer = function(options)
 
 WMSLayer.DEAFULT_PARAM = {
 	SERVICE     : 'WMS',
-	VERSION     : '1.3.0',
+	VERSION     : '1.1.1',
 	REQUEST     : 'GetMap',
-	SRS         : 'EPSG:4326',
-	CRS         : 'EPSG:4326',
+	SRS         : 'EPSG:3857',
+	CRS         : 'EPSG:3857',
 	WIDTH       : 256,
 	HEIGHT      : 256,
 	FORMAT      : 'image/png',
@@ -57,12 +57,22 @@ WMSLayer.prototype.getUrl = function(info)
 	var minGeographicCoord = rectangle.minGeographicCoord;
 	var maxGeographicCoord = rectangle.maxGeographicCoord;
 
+	// Test to convert coords to meters.***********************************
+	var minMercator = Globe.geographicToMercatorProjection(minGeographicCoord.longitude, minGeographicCoord.latitude, undefined);
+	var maxMercator = Globe.geographicToMercatorProjection(maxGeographicCoord.longitude, maxGeographicCoord.latitude, undefined);
+	// End test.-----------------------------------------------------------
+
 	var isLatest = this._requestParam.get('VERSION') === '1.3.0';
-	
+	/*
 	var minx = isLatest ? minGeographicCoord.latitude : minGeographicCoord.longitude;
 	var miny = isLatest ? minGeographicCoord.longitude : minGeographicCoord.latitude;
 	var maxx = isLatest ? maxGeographicCoord.latitude : maxGeographicCoord.longitude;
 	var maxy = isLatest ? maxGeographicCoord.longitude : maxGeographicCoord.latitude;
+	*/
+	var minx = minMercator.x;
+	var miny = minMercator.y;
+	var maxx = maxMercator.x;
+	var maxy = maxMercator.y;
 	var bbox = minx + ',' + miny + ',' + maxx + ',' + maxy;
 
 	this._requestParam.set('BBOX', bbox);
