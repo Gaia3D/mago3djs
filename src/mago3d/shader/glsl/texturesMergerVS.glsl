3b765d30e0cce17bed852e9bc7f1c7f0b34c0be9
kwunjaehyun <shinekill3@gmail.com>
2020-06-08 21:01:08 +0900

    오타 수정

--- src/mago3d/shader/glsl/texturesMergerVS.glsl
+++ src/mago3d/shader/glsl/texturesMergerVS.glsl
@@ -7,5 +7,5 @@ varying vec2 v_tex_pos;
 void main() {
     v_tex_pos = a_pos;
     //vec2 pos = a_pos*0.5;
-    gl_Position = vec4(1.0 - 2.0 * pos, 0, 1);
+    gl_Position = vec4(1.0 - 2.0 * a_pos, 0, 1);
 }
