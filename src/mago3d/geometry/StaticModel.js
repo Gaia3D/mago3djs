07c32887b7ce25de55e8631656031f8181367200
kwunjaehyun <shinekill3@gmail.com>
2020-09-15 01:45:17 +0900

    staticmodel 바운딩박스 처음에만 제대로 나오는 문제 해결

--- src/mago3d/geometry/StaticModel.js
+++ src/mago3d/geometry/StaticModel.js
@@ -96,68 +96,69 @@ StaticModelsManager.manageStaticModel = function(node, magoManager)
 		var neoBuildingFolderName;
 	
 		// check if has neoBuilding.***
-		if (neoBuilding === undefined)
-		{
-			var neoBuildingFolderName = attributes.buildingFolderName;
-			projectFolderName = attributes.projectFolderName;
-			
-			// demand to staticModelsManager the neoBuilding.***
-			var projectId = attributes.projectId;
-			var staticModelsManager = magoManager.hierarchyManager.getStaticModelsManager();
-			var staticModel = staticModelsManager.getStaticModel(projectId);
-			neoBuilding = staticModel.neoBuilding;
-			neoBuildingFolderName = staticModel.buildingFolderName;
-			projectFolderName = staticModel.projectFolderName;
-			
-			//neoBuilding = staticModelsManager.getStaticModel(staticModelDataPath);
-			
-			// make a buildingSeed.***
-			var buildingSeed = new BuildingSeed();
-			buildingSeed.fisrtName = neoBuildingFolderName;
-			buildingSeed.name = neoBuildingFolderName;
-			buildingSeed.buildingId = neoBuildingFolderName;
-			buildingSeed.buildingFileName = neoBuildingFolderName;
-			buildingSeed.geographicCoord = new GeographicCoord(attributes.longitude, attributes.latitude, attributes.height); // class : GeographicCoord.
-			buildingSeed.rotationsDegree = new Point3D(attributes.pitch, attributes.roll, attributes.heading); // class : Point3D. (heading, pitch, roll).
-			buildingSeed.bBox = new BoundingBox();           // class : BoundingBox.
-			buildingSeed.bBox.init();
-			buildingSeed.bBox.expand(10.0); // we dont know the bbox size, provisionally set as 10,10,10.***
-			buildingSeed.geographicCoordOfBBox = new GeographicCoord(attributes.longitude, attributes.latitude, attributes.height);  // class : GeographicCoord.
-			buildingSeed.smartTileOwner;
-			
-			// Now, set neoBuildings parameters.***
-			neoBuilding.buildingFileName = neoBuildingFolderName;
-			neoBuilding.nodeOwner = node;
-			node.data.neoBuilding = neoBuilding;
-			node.data.buildingSeed = buildingSeed;
-			node.data.projectFolderName = projectFolderName;
+		if(neoBuilding || neoBuilding instanceof NeoBuilding) return;
 
-			if (neoBuilding.metaData === undefined) 
-			{ 
-				neoBuilding.metaData = new MetaData(); 
-				
-				if (neoBuilding.metaData.geographicCoord === undefined)
-				{ neoBuilding.metaData.geographicCoord = new GeographicCoord(); }
-
-				if (neoBuilding.metaData.bbox === undefined) 
-				{ neoBuilding.metaData.bbox = new BoundingBox(); }
-			
-				neoBuilding.metaData.geographicCoord.setLonLatAlt(buildingSeed.geographicCoord.longitude, buildingSeed.geographicCoord.latitude, buildingSeed.geographicCoord.altitude);
-				neoBuilding.metaData.bbox.copyFrom(buildingSeed.bBox);
-				neoBuilding.metaData.heading = buildingSeed.rotationsDegree.z;
-				neoBuilding.metaData.pitch = buildingSeed.rotationsDegree.x;
-				neoBuilding.metaData.roll = buildingSeed.rotationsDegree.y;
-			}
-
-			neoBuilding.name = "test_" + neoBuildingFolderName;
-			neoBuilding.buildingId = neoBuildingFolderName;
+		var neoBuildingFolderName = attributes.buildingFolderName;
+		projectFolderName = attributes.projectFolderName;
 		
-			neoBuilding.buildingType = "basicBuilding";
+		// demand to staticModelsManager the neoBuilding.***
+		var projectId = attributes.projectId;
+		var staticModelsManager = magoManager.hierarchyManager.getStaticModelsManager();
+		var staticModel = staticModelsManager.getStaticModel(projectId);
+		neoBuilding = staticModel.neoBuilding;
+		neoBuildingFolderName = staticModel.buildingFolderName;
+		projectFolderName = staticModel.projectFolderName;
+		
+		//neoBuilding = staticModelsManager.getStaticModel(staticModelDataPath);
+		
+		// make a buildingSeed.***
+		var buildingSeed = new BuildingSeed();
+		buildingSeed.fisrtName = neoBuildingFolderName;
+		buildingSeed.name = neoBuildingFolderName;
+		buildingSeed.buildingId = neoBuildingFolderName;
+		buildingSeed.buildingFileName = neoBuildingFolderName;
+		buildingSeed.geographicCoord = new GeographicCoord(attributes.longitude, attributes.latitude, attributes.height); // class : GeographicCoord.
+		buildingSeed.rotationsDegree = new Point3D(attributes.pitch, attributes.roll, attributes.heading); // class : Point3D. (heading, pitch, roll).
+		buildingSeed.bBox = new BoundingBox();           // class : BoundingBox.
+		buildingSeed.bBox.init();
+		buildingSeed.bBox.expand(10.0); // we dont know the bbox size, provisionally set as 10,10,10.***
+		buildingSeed.geographicCoordOfBBox = new GeographicCoord(attributes.longitude, attributes.latitude, attributes.height);  // class : GeographicCoord.
+		buildingSeed.smartTileOwner;
+		
+		// Now, set neoBuildings parameters.***
+		neoBuilding.buildingFileName = neoBuildingFolderName;
+		neoBuilding.nodeOwner = node;
+		node.data.neoBuilding = neoBuilding;
+		node.data.buildingSeed = buildingSeed;
+		node.data.projectFolderName = projectFolderName;
+
+		if (neoBuilding.metaData === undefined) 
+		{ 
+			neoBuilding.metaData = new MetaData(); 
+			
+			if (neoBuilding.metaData.geographicCoord === undefined)
+			{ neoBuilding.metaData.geographicCoord = new GeographicCoord(); }
+
+			if (neoBuilding.metaData.bbox === undefined) 
+			{ neoBuilding.metaData.bbox = new BoundingBox(); }
+		
+			neoBuilding.metaData.geographicCoord.setLonLatAlt(buildingSeed.geographicCoord.longitude, buildingSeed.geographicCoord.latitude, buildingSeed.geographicCoord.altitude);
+			neoBuilding.metaData.bbox.copyFrom(buildingSeed.bBox);
+			neoBuilding.metaData.heading = buildingSeed.rotationsDegree.z;
+			neoBuilding.metaData.pitch = buildingSeed.rotationsDegree.x;
+			neoBuilding.metaData.roll = buildingSeed.rotationsDegree.y;
+
 			if (neoBuilding.bbox === undefined)
 			{ neoBuilding.bbox = new BoundingBox(); }
 			neoBuilding.bbox.copyFrom(buildingSeed.bBox);
-			neoBuilding.projectFolderName = node.data.projectFolderName;
+		} else {
+			neoBuilding.bbox = neoBuilding.metaData.bbox;
 		}
+
+		neoBuilding.name = "test_" + neoBuildingFolderName;
+		neoBuilding.buildingId = neoBuildingFolderName;
+		neoBuilding.buildingType = "basicBuilding";
+		neoBuilding.projectFolderName = node.data.projectFolderName;
 	}
 };
 
